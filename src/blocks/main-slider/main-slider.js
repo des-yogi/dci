34d9de92ef1785cea788a7212c68902893bddbc4
Igor <nuevo-design@mail.ru>
2019-06-17 15:33:49 +0300

    Add files via upload

---
+++ src/blocks/main-slider/main-slider.js
@@ -0,0 +1,13 @@
+(function () {// code
+  Array.prototype.forEach.call( document.querySelectorAll('.main-slider__title > a'), function(title) {
+    var titleArr = title.innerText.split(' ');
+    var newArr =  titleArr.map(function(el, idx) {
+      if (idx === 0 || (idx % 2 === 0) ) {
+        return el.innerHTML = '<span style="color: red;">' + el + '</span>';
+      } else return el;
+    }).join(' ');
+    title.innerHTML = newArr;
+      //console.log( typeof(title.innerHTML) );
+  });
+
+})();
